59e37304d2a927c35c68947f503dbd8773911eea
Marco <antnyhern@gmail.com>
2021-06-13 22:42:26 -0500

    Add all proyect

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ChargeDataComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(287, 33);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RFC";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "SuggesAppend"});
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.Location = new System.Drawing.Point(372, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(62, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(612, 199);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 43);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChargeDataComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AutoComplete();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Data Source=192.168.1.65;User Id=sa;Password=N6AZwKXNzI62xr+hCknvlQ==;Connection Timeout=10000;
+            SqlConnection connection = new SqlConnection("Data Source=10.0.0.87,14033; User Id=sa; Password=Passw0rd;Connection Timeout=300;");
+            SqlCommand command = new SqlCommand("SELECT [username] from [SgInvoiceV5_ant].[dbo].[CE_comprobantes]", connection);
+            connection.Open();
+            SqlDataReader registers = command.ExecuteReader();
+            while (registers.Read())
+            {
+                if (comboBox1.Items.Contains(registers["username"].ToString()))
+                {
+
+                }
+                else
+                {
+                    comboBox1.Items.Add(registers["username"].ToString());
+                }
+                
+            }
+        }
+
+        void AutoComplete()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=10.0.0.87,14033; User Id=sa; Password=Passw0rd;Connection Timeout=300;");
+            DataTable TableData = new DataTable();
+
+            AutoCompleteStringCollection list = new AutoCompleteStringCollection();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT [receptor_nombre] from [SgInvoiceV5_ant].[dbo].[CE_comprobantes]", connection);
+            adapter.Fill(TableData);
+
+            for (int i = 0; i < TableData.Rows.Count; i++)
+            {
+                list.Add(TableData.Rows[i]["receptor_nombre"].ToString());
+            }
+
+            textBox1.AutoCompleteCustomSource = list;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string User =  comboBox1.Text;
+            string Name = textBox1.Text;
+            if (User.Equals(null) || User.Equals("") || Name.Equals(null) || Name.Equals(""))
+            {
+                MessageBox.Show("The search need the fields User and Name");
+            }
+            else
+            {
+                SqlConnection connection = new SqlConnection("Data Source=10.0.0.87,14033; User Id=sa; Password=Passw0rd;Connection Timeout=300;");
+                
+                SqlCommand command = new SqlCommand("SELECT * from [SgInvoiceV5_ant].[dbo].[CE_comprobantes] where [receptor_nombre]='" + Name + "' and [username]='" + User + "'", connection);
+                SqlDataAdapter Adapter = new SqlDataAdapter();
+                Adapter.SelectCommand = command;
+                DataTable TableDat = new DataTable();
+                Adapter.Fill(TableDat);
+                dataGridView1.DataSource = TableDat;
+            }
+        }
+    }
+}
